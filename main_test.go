6f21676ff70d5e62eba8e65d2c0d5b745bb1aa05
Jonathan M <jo-m@outlook.com>
2026-05-19 06:50:34 +0200

    fixup! filter by timestamp

--- main_test.go
+++ main_test.go
@@ -15,7 +15,7 @@ func TestKeepEarliestPerLocation(t *testing.T) {
 		{LocationID: 1, ParameterID: 7, Timestamp: t0, Value: 10}, // earliest for loc 1
 		{LocationID: 1, ParameterID: 7, Timestamp: t0.Add(time.Hour), Value: 15},
 		{LocationID: 2, ParameterID: 7, Timestamp: t0.Add(3 * time.Hour), Value: 30},
-		{LocationID: 2, ParameterID: 7, Timestamp: t0.Add(time.Hour), Value: 12}, // earliest for loc 2
+		{LocationID: 2, ParameterID: 7, Timestamp: t0.Add(time.Hour), Value: 12},     // earliest for loc 2
 		{LocationID: 3, ParameterID: 7, Timestamp: t0.Add(5 * time.Hour), Value: 99}, // only entry for loc 3
 	}
 	got := keepEarliestPerLocation(rows)
